eb9301550c4dc60b385efcebe62abe112b50b947
Bonusree <bonusreedatta820@gmail.com>
2022-12-04 20:47:05 +0600

    segment_tree

--- Algorithm/segment_tree.cpp
+++ Algorithm/segment_tree.cpp
@@ -1,106 +1,90 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define mx 200000
-#define  lli long long int
-lli tr[mx],trr[mx*3];
-void init(lli pos,lli suru,lli ses)
+#define lli int
+const long long int lim=10e5+3;
+lli arr[lim],tree[lim*3];
+void init(lli node, lli start, lli end)
 {
-    if(suru==ses)
-    {
-        trr[pos]=tr[ses];
-        return;
-    }
-    lli mid=(suru+ses)/2,bam=pos*2,dan=pos*2+1;
-    init(bam,suru,mid);
-    init(dan,mid+1,ses);
-    trr[pos]=(trr[bam]+trr[dan]);
+  if(start==end)
+  {
+    tree[node]=arr[start];
+    return;
+  }
+  lli mid=(start+end)/2;
+  lli left=node*2;
+  lli right=node*2+1;
+  init(left,start,mid);
+  init(right,mid+1,end);
+  tree[node]=tree[left]+tree[right];
 }
-void update(lli pos,lli suru,lli ses,lli up,lli val)
+void update(lli node,lli start,lli end,lli i,lli val)
 {
-    lli i;
-    if(up<suru||up>ses)
-    {
-        return;
-    }
-    if(up<=suru&&up>=ses)
-    {
-        trr[pos]+=val;
-        return;
-    }
-    lli mid=(suru+ses)/2,bam=pos*2,dan=pos*2+1;
-    update(bam,suru,mid,up,val);
-    update(dan,mid+1,ses,up,val);
-    trr[pos]=trr[bam]+trr[dan];
+  if(i<start||i>end)
+  return;
+  else if(i==start&&i==end)
+  {
+   // cout<<tree[start]<<endl;
+    tree[node]=val;
+    return;
+  }
+  lli mid=(start+end)/2;
+  lli left=node*2;
+  lli right=node*2+1;
+  update(left,start,mid,i,val);
+  update(right,mid+1,end,i,val);
+  tree[node]=tree[right]+tree[left];
+
 }
-void update1(lli pos,lli suru,lli ses,lli up)
+
+lli query(lli node,lli start,lli end,lli l,lli r)
 {
-    lli i;
-    if(up<suru||up>ses)
-    {
-        return;
-    }
-    else if(up<=suru&&up>=ses)
-    {
-        cout<<trr[pos]<<endl;
-        trr[pos]=0;
-        return;
-    }
-    lli mid=(suru+ses)/2,bam=pos*2,dan=pos*2+1;
-    update1(bam,suru,mid,up);
-    update1(dan,mid+1,ses,up);
-    trr[pos]=trr[bam]+trr[dan];
-}
-lli query(lli pos,lli suru,lli ses,lli up,lli low)
-{
-    if(up>ses||low<suru)
-    {
-        return 0;
-    }
-    else if(suru>=up&&ses<=low)
-    {
-        
-       return trr[pos];
-    }
-    lli mid=(suru+ses)/2,bam=pos*2,dan=pos*2+1;
-    lli q1=query(bam,suru,mid,up,low);
-    lli q2=query(dan,mid+1,ses,up,low);
-    return (q1+q2);
+  lli m=0;
+  if(r<start||l>end)
+  return m;
+  else if(l<=start&&r>=end)
+  return tree[node];
+  lli mid=(start+end)/2;
+  lli left=node*2;
+  lli right=node*2+1;
+  lli q1=query(left,start,mid,l,r);
+  lli q2=query(right,mid+1,end,l,r);
+  return q1+q2;
 }
 int main()
 {
-    lli t,cas=0;
-    cin>>t;
-    while(t--)
+   ios_base::sync_with_stdio(false);
+   cin.tie(0);
+   cout.tie(0);
+    lli t=1, cases = 0;
+   cin>>t;
+    while (t--)
     {
-        cas++;
-    long long int n,q,x,v,i,j,e=0,o=0,m,y,p,a,b;
-    char c1,c2;
-    string str;
-    memset(trr,0,sizeof trr);
-    cin>>n>>m;
-    for(i=1;i<n+1;i++)
-    cin>>tr[i];
-    init(1,1,n);
-    cout<<"Case "<<cas<<":"<<endl;
-    for(i=0;i<m;i++)
-    {
+      cases++;
+      lli n,m;
+      cin>>n>>m;
+      memset(tree,0,n+2);
+      for(lli i=1;i<=n;i++)
+      cin>>arr[i];
+      init(1,1,n);
+      cout<<"Case "<<cases<<":"<<endl;
+      while(m--)
+      {
+        lli a;
         cin>>a;
         if(a==1)
         {
-            cin>>x;
-            (update1(1,1,n,x+1));
+          lli b,c;
+          cin>>b>>c;
+          update(1,1,n,b+1,c);
         }
-        else if(a==2)
-        {
-            cin>>x>>y;
-            update(1,1,n,x+1,y);
-        }
+        
         else
         {
-            cin>>x>>y;
-            cout<<query(1,1,n,x+1,y+1)<<endl;
+          lli b,c;
+          cin>>b>>c;
+          cout<<query(1,1,n,b+1,c+1)<<endl;
         }
+      }
+
     }
-    }
-   
 }
